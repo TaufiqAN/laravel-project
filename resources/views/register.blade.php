362b38a94f40cee5033f91d40170ca9f4dabb95a
TaufiqAN <taufiqadi02626@gmail.com>
2023-10-06 08:19:52 +0700

    create Register View

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,25 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        footer {
-    color: white;
-    background-color: rgb(36, 36, 36);
-    text-align: center;
-    padding: 30px;
-    }
-    * {
-    padding: 0;
-    margin: 0;
-    }
-
-    </style>
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
-    <!-- Konten -->
-    <footer>
-        Edited by Taufiq &copy;2023
-    </footer>
+    <!-- Register -->
+    <div class="position-absolute top-50 start-50 translate-middle" style="border: 1px solid black; padding:3rem; border-radius: 6px">
+      <h2>Register</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="name" placeholder="Name">
+                    </div>
+                    <div class="mb-3">
+                      <label for="email" class="form-label">Email</label>
+                      <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Email">
+                    </div>
+                    <div class="mb-3">
+                      <label for="password" class="form-label">Password</label>
+                      <input type="password" class="form-control" id="password" aria-describedby="password" placeholder="Password">
+                    </div>
+                    <div class="mb-3">
+                      <label for="confirmpassword" class="form-label">Confirm Password</label>
+                      <input type="confirmpassword" class="form-control" id="confirmpassword" aria-describedby="confirmpassword" placeholder="Confirm Password">
+                    </div>c
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                  </form>
+            </div>
+        </div>
+      </div>
+  </div>
+<!-- Register End -->
 </body>
 </html>
